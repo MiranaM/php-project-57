87629e2ba55d7d768a92e0ffeff14f697075cded
MiranaM <0resama@mail.ru>
2025-06-29 02:18:21 +0300

    text

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,26 +1,20 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-    </div>
-
     <form method="POST" action="{{ route('password.confirm') }}">
         @csrf
 
         <!-- Password -->
         <div>
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Пароль')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <div class="flex justify-end mt-4">
-            <x-primary-button>
-                {{ __('Confirm') }}
+            <x-primary-button px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 text-sm>
+                {{ __('Подтвердить') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,11 +34,11 @@
                 @else
                 <a href="{{ route('login') }}"
                     class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 text-sm">
-                    Войти
+                    Вход
                 </a>
                 <a href="{{ route('register') }}"
                     class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 text-sm">
-                    Зарегистрировать
+                    Регистрация
                 </a>
                 @endauth
             </div>
