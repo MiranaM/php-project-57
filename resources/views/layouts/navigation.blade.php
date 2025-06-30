eef95a012dff70ac7ba0fd2a30dd28290754767a
MiranaM <0resama@mail.ru>
2025-06-30 13:56:48 +0300

    exit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -27,7 +27,7 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
                     <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 text-sm">
-                        Выйти
+                        Выход
                     </button>
                 </form>
                 @else
